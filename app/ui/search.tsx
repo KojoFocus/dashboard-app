a73b89a48262dde2e62e8809e83b2c43253323a6
KojoFocus <ernestobimpeh9@gmail.com>
2024-08-24 08:17:26 +0000

    added pagenation

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   // Debounced search function
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams as any);
+    params.set('page', '1');
     if (term) {
       params.set("query", term);
     } else {
